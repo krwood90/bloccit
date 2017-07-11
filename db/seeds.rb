3bb9efef60beb34f2381f3c11065e7efd3c044fd
Kevin Wood <krwood90@gmail.com>
2017-07-11 14:25:00 +0000

    created user and admin roles, the rspec still messing up

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,9 +38,18 @@
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'krwood90@gmail.com',
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
  
